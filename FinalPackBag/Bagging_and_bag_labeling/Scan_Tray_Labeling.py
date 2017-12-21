ede8201908ca2c1b8402eeffd812115f04aaa11a
blackroad99 <vladvell@gmail.com>
2017-12-21 09:34:45 -0600

    test
    aa

--- FinalPackBag/Bagging_and_bag_labeling/Scan_Tray_Labeling.py
+++ FinalPackBag/Bagging_and_bag_labeling/Scan_Tray_Labeling.py
@@ -9,17 +9,17 @@ def test_scan_rfid_epc():
         # click 'start'
         AAEC.main_app_start()
         AAEC.log_in()
-        AAEC.printer_label_dialog.wait('visible ready enabled',20,1)
+        AAEC.printer_label_dialog.wait('visible ready enabled',20,2)
         AAEC.printer_label_dialog.Yes.click()
         AAEC.rfid_manual_submit(value=['A00001'])
         AAEC.lot_input(
-            lot_id=['MA12836A009,L3ASY164'])
+            lot_id=['MA12836A009,L3ASY166'])
         AAEC.chumber_ready()
         AAEC.rfid_manual_submit() #Bag integrity check
         AAEC.rfid_manual_submit(value=['A00001'])
         AAEC.rfid_manual_submit(value=['B00001'])
         AAEC.lot_input(
-            lot_id=['MA12836A009,L3ASY164'])
+            lot_id=['MA12836A009,L3ASY166'])
         AAEC.chumber_ready() #Bag integrity check
         AAEC.rfid_manual_submit(value=['B00001'])
         AAEC.rfid_manual_submit(value=['B00001', 'A00001'])
